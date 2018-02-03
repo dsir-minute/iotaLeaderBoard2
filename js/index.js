03798bafe602199cfc294ad3f9829eb87189cea1
raxy <dsir-minute@gmail.com>
2018-02-03 16:54:04 +0100

    better message handling, status now in main table

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // I am  leaderboard-example/js/index.js from https://github.com/domschiener/leaderboard-example.git
-// modded by raxy, last upd on 01feb18, beautified by http://jsbeautifier.org/
+// modded by raxy, last upd on 02feb18, beautified by http://jsbeautifier.org/
 // uses iota.js v0.4.7
 
 function formattedNow( unixMillis){
@@ -65,8 +65,9 @@ $(document).ready(function() {
    //
    // display transfers à la raxycli-app, to be debugged
    //
-   function showItems( number, accountData) { 
-      const transfers = accountData.transfers.slice(0);
+   function showItems( accountData) { 
+      var transferList = [];
+      const transfers = accountData.transfers.slice(0).reverse();
       const categorizedTransfers = iota.utils.categorizeTransfers(transfers, accountData.addresses);
 
       const biggestValue = transfers.reduce(
@@ -92,10 +93,40 @@ $(document).ready(function() {
          const value = bundle[0].value;
 
          const thisCategorizeTransfer = categorizedTransfers.sent.filter(t => t[0].hash === bundle[0].hash);
-         const type = bundle.length === 1 && bundle[0].value === 0 ? 'address' : thisCategorizeTransfer.length > 0 ? 'spending from' : 'receiving to';
+         const type = (bundle.length === 1 && bundle[0].value === 0 ? 'address' : (thisCategorizeTransfer.length > 0 ? 'spending from' : 'receiving to'));
          console.log( "hash="+shortHash+","+type+"="+shortAddress+",value="+value);
-         console.log( "status="+(persisted ? " confirmed " : reattachConfirmed ? " bundle confirmed " : " pending ")+",time="+time);
+         var theStatus = (persisted ? " confirmed " : reattachConfirmed ? " bundle confirmed " : " pending ");
+         console.log( "status="+theStatus+",time="+time);
+
+         var message = iota.utils.extractJson(bundle);
+         if( message){
+            console.log("Extracted msg from transfer: ", message);
+            //var jsonMessage = JSON.parse(message);
+            //console.log("JSON: ", message);
+            var newTx = {
+//               'name': 'none',
+               'message': message,
+               'value': bundle[0].value,
+               'persistence': theStatus,
+               'hash': bundle[0].hash+"<br/>"+bundle[0].bundle,
+               'tstamp': bundle[0].attachmentTimestamp
+            }
+         } else {
+            console.log("transfer did not contain message");
+            var newTx = {
+//               'name': 'none',
+               'message': 'none',
+               'value': 'none',
+               'persistence': theStatus,
+               'hash': bundle[0].hash+"<br/>"+bundle[0].bundle,
+               'tstamp': bundle[0].attachmentTimestamp
+            }
+         }
+         transferList.push(newTx);
+
       });
+      updateLeaderboardHTML(transferList);
+
    };
   
    //
@@ -103,10 +134,10 @@ $(document).ready(function() {
    //  As well as the current balance
    //  Automatically updates the HTML on the site
    //
-   function getAccountInfo() {
+   function getAccountInfoOld() {
       // Command to be sent to the IOTA Node
       // Gets the latest transfers for the specified seed
-      console.log("in getAccountInfo() for seed "+seed);
+      console.log("in getAccountInfoOld() for seed "+seed);
       iota.api.getAccountData(seed, function(e, accountData) {
          console.log("Account data", accountData);
          // Update address in case it's not defined yet
@@ -130,8 +161,8 @@ $(document).ready(function() {
                      'name': 'none',
                      'message': message,
                      'value': transfer[0].value,
-                     'persistence': transfer[0].persistence, // TODO : clone processing of history.js in raxycli-app repo to handle 'bundle-confirmed' & co
-                     'hash': transfer[0].hash+"/"+transfer[0].bundle,
+                     'persistence': null,
+                     'hash': transfer[0].hash+"<br/>"+transfer[0].bundle,
                      'tstamp': transfer[0].attachmentTimestamp
                   }
                } else {
@@ -140,8 +171,8 @@ $(document).ready(function() {
                      'name': 'none',
                      'message': 'none',
                      'value': 'none',
-                     'persistence': transfer[0].persistence,
-                     'hash': transfer[0].hash+"/"+transfer[0].bundle,
+                     'persistence': null,
+                     'hash': transfer[0].hash+"<br/>"+transfer[0].bundle,
                      'tstamp': transfer[0].attachmentTimestamp
                   }
                }
@@ -150,15 +181,28 @@ $(document).ready(function() {
             updateLeaderboardHTML(transferList);
             // Increase the counter of checkedTxs
             //checkedTxs = accountData.transfers.length;
-            showItems( 10, accountData);
+//            showItems( accountData);
          }
+      })
+   }
+   function getAccountInfoNew() {
+      // Command to be sent to the IOTA Node
+      // Gets the latest transfers for the specified seed
+      console.log("in getAccountInfoNew() for seed "+seed);
+      iota.api.getAccountData(seed, function(e, accountData) {
+         console.log("Account data", accountData);
+         // Update address in case it's not defined yet
+         if (!address && accountData.addresses[0]) {
+            address = iota.utils.addChecksum(accountData.addresses[accountData.addresses.length - 1]);
+            updateAddressHTML(address);
+         }
+         showItems( accountData);
       })
    }
    //
    //  Updates the leaderboard list HTML
    //
    function updateLeaderboardHTML(rankedList) {
-
        // Now we actually sort the rankedList
        rankedList.sort(function (a, b) {
            if (a.value > b.value) {
@@ -183,15 +227,16 @@ $(document).ready(function() {
        for (var i = 0; i < rankedList.length; i++) {
 
            var message = JSON.stringify(rankedList[i].message);
-           var name = rankedList[i].name;
+//           var name = rankedList[i].name;
            var value = rankedList[i].value
            var rank = i + 1;
+           var status = rankedList[i].persistence;
 
-           var listElement = '<tr><td>' + rank + '</td><td>'+formattedNow( rankedList[i].tstamp)+'</td><td>' + message + '</td><td>' + value + '</td></tr>'
+           var listElement = '<tr><td>' + rank + '</td><td>'+formattedNow( rankedList[i].tstamp)+'</td><td>' + message + '</td><td>' + value + '</td>';
+           listElement += '<td>' + status + '</td></tr>';
            html += listElement;
            htmlConfirms += '<tr><td>'+ rank +'</td>';
-           htmlConfirms += '<td>'+ rankedList[i].hash +'</td>';
-           htmlConfirms += '<td>'+ rankedList[i].persistence +'</td></tr>';
+           htmlConfirms += '<td>'+ rankedList[i].hash +'</td></tr>';
        }
 
        $("#leaderboard").html(html);
@@ -217,8 +262,8 @@ $(document).ready(function() {
       //$("#enterSeed").html('<div class="alert alert-success" role="alert">Tks for your connection params. You can generate an address now.</div>');
       $(this).hide();
       // We fetch the latest transactions now & every 90 seconds
-      getAccountInfo(); 
-      //setInterval(getAccountInfo, 90000);
+      getAccountInfoNew(); 
+      setInterval(getAccountInfoNew, 90000);
       alert("Tks for your connection params. You can generate an address now.");
    });
    //
